1d7b3e5f4a8d45fda7b9bb8e536a7c3a0c623b84
Antonio González <antonio.gonzalez@codeoscopic.com>
2023-03-23 17:59:54 +0100

    fix: add new project configuration

--- src/db.js
+++ src/db.js
@@ -15,7 +15,7 @@ import {
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-  databaseURL: import.meta.env.VITE_DATABASE_URL,
+  // databaseURL: import.meta.env.VITE_DATABASE_URL,
   projectId: import.meta.env.VITE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
@@ -69,5 +69,6 @@ export async function getCategories() {
   snapshot.forEach((doc) => {
     categories.push({ ...doc.data(), id: doc.id });
   });
+  console.log(categories);
   return categories;
 }
